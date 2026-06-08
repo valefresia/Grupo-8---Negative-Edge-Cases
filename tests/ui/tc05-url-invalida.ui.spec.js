03881a7360a5f4d1ecb3e3d0d96c9ebf45ef5bbd
valefresia <131785137+valefresia@users.noreply.github.com>
2026-06-08 18:35:49 -0300

    correccion tc05

--- tests/ui/tc05-url-invalida.ui.spec.js
+++ tests/ui/tc05-url-invalida.ui.spec.js
@@ -4,8 +4,15 @@ test.describe('TC05 - Navegación a URL inválida maneja error UI', () => {
   test('UI: acceder a una URL inexistente muestra mensaje de error', async ({
     page,
   }) => {
-    await page.goto('/aaaa');
+    // Usamos la URL completa porque la relativa falla sin baseURL activo
+    await page.goto('https://www.demoblaze.com/paginaquenova', {
+      waitUntil: 'commit',
+    });
 
+    // Esperamos que el body tenga contenido
+    await expect(page.locator('body')).not.toBeEmpty();
+
+    // Verificamos que muestra el mensaje de errornpx
     await expect(page.locator('body')).toContainText('Not Found');
   });
 });
