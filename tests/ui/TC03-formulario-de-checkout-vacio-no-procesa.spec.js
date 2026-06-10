3f1e482b241dbbb1ac30b1147ab1f22c2a64741b
Pamela <paguirre739@gmail.com>
2026-06-10 18:29:35 -0300

    TC03 - modificaciones

--- tests/ui/TC03-formulario-de-checkout-vacio-no-procesa.spec.js
+++ tests/ui/TC03-formulario-de-checkout-vacio-no-procesa.spec.js
@@ -1,15 +1,79 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
-  //Con usuario logueado, ingresar al carrito y clickear en Place Order sin completar el formulario, verificar que no se procesa la compra y se muestra un mensaje de error
+test('TC03.1 - Name vacio y Credit Card vacio', async ({ page }) => {
   await page.goto('https://demoblaze.com/index.html');
-
-  await page.getByRole('link', { name: 'Cart' }).click();
+  await page.getByRole('link', { name: 'Nokia lumia' }).click();
+  page.once('dialog', (dialog) => {
+    console.log(`Dialog message: ${dialog.message()}`);
+    dialog.dismiss().catch(() => {});
+  });
+  await page.getByRole('link', { name: 'Add to cart' }).click();
+  await page.getByRole('link', { name: 'Cart', exact: true }).click();
   await page.getByRole('button', { name: 'Place Order' }).click();
   page.once('dialog', (dialog) => {
     console.log(`Dialog message: ${dialog.message()}`);
     dialog.dismiss().catch(() => {});
   });
+  await page.getByRole('button', { name: 'Purchase' }).click();
+});
 
+test('TC03.2 - Name completo y Credit Card vacio', async ({ page }) => {
+  await page.goto('https://demoblaze.com/index.html');
+  await page.getByRole('link', { name: 'Samsung galaxy s6' }).click();
+  page.once('dialog', (dialog) => {
+    console.log(`Dialog message: ${dialog.message()}`);
+    dialog.dismiss().catch(() => {});
+  });
+  await page.getByRole('link', { name: 'Add to cart' }).click();
+  await page.getByRole('link', { name: 'Cart', exact: true }).click();
+  await page.getByRole('button', { name: 'Place Order' }).click();
+  await page.getByRole('textbox', { name: 'Total: 360 Name:' }).click();
+  await page
+    .getByRole('textbox', { name: 'Total: 360 Name:' })
+    .fill('Pamela Aguirre');
+  page.once('dialog', (dialog) => {
+    console.log(`Dialog message: ${dialog.message()}`);
+    dialog.dismiss().catch(() => {});
+  });
+  await page.getByRole('button', { name: 'Purchase' }).click();
+});
+
+test('TC03.3 - Name vacio y Credit Card completo', async ({ page }) => {
+  await page.goto('https://demoblaze.com/index.html');
+  await page.getByRole('link', { name: 'Sony xperia z5' }).click();
+  page.once('dialog', (dialog) => {
+    console.log(`Dialog message: ${dialog.message()}`);
+    dialog.dismiss().catch(() => {});
+  });
+  await page.getByRole('link', { name: 'Add to cart' }).click();
+  await page.getByRole('link', { name: 'Cart', exact: true }).click();
+  await page.getByRole('button', { name: 'Place Order' }).click();
+  await page.getByRole('textbox', { name: 'Credit card:' }).click();
+  await page.getByRole('textbox', { name: 'Credit card:' }).fill('25252545');
+  page.once('dialog', (dialog) => {
+    console.log(`Dialog message: ${dialog.message()}`);
+    dialog.dismiss().catch(() => {});
+  });
+  await page.getByRole('button', { name: 'Purchase' }).click();
+});
+
+test('TC03.4 - Name y Credit Card completos', async ({ page }) => {
+  await page.goto('https://demoblaze.com/index.html');
+  await page.getByRole('link', { name: 'Samsung galaxy s7' }).click();
+  page.once('dialog', (dialog) => {
+    console.log(`Dialog message: ${dialog.message()}`);
+    dialog.dismiss().catch(() => {});
+  });
+  await page.getByRole('link', { name: 'Add to cart' }).click();
+  await page.getByRole('link', { name: 'Cart', exact: true }).click();
+  await page.getByRole('button', { name: 'Place Order' }).click();
+  await page.getByRole('textbox', { name: 'Total: 800 Name:' }).click();
+  await page
+    .getByRole('textbox', { name: 'Total: 800 Name:' })
+    .fill('Pamela Aguirre');
+  await page.getByRole('textbox', { name: 'Credit card:' }).click();
+  await page
+    .getByRole('textbox', { name: 'Credit card:' })
+    .fill('2525454569697878');
   await page.getByRole('button', { name: 'Purchase' }).click();
 });
